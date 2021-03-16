da62848781b4d4edb464463b5cfd8c41f6af1aab
Lody Borgers <hi@lodybo.nl>
2021-03-16 23:41:12 +0100

    Fixed some missing Gatsby config

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,41 +1,42 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require('dotenv').config({
+  path: `.env.${ process.env.NODE_ENV }`,
+});
 
 const { projectId } = require('./sanity.json');
 
 module.exports = {
   siteMetadata: {
-    title: "Weddingfest",
+    title: 'Weddingfest',
+    siteUrl: 'https://weddingfest.nl'
   },
   plugins: [
     {
-      resolve: "gatsby-source-sanity",
+      resolve: 'gatsby-source-sanity',
       options: {
         projectId,
         dataset: process.env.GATSBY_SANITY_DATASET,
       },
     },
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-netlify",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
