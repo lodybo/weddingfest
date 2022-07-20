2343cb78b065276199ee5691eb33bd73accd1233
Lody Borgers <l.borgers@taf.nl>
2022-07-20 18:51:09 +0200

    Initial commit, with some changes

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: {
+        light: colors.cyan['50'],
+        DEFAULT: colors.cyan['100'],
+        dark: colors.cyan['200'],
+      },
+      secondary: {
+        light: colors.rose['50'],
+        DEFAULT: colors.rose['100'],
+        dark: colors.rose['200'],
+      },
+      white: colors.stone['50'],
+      black: colors.stone['800'],
+    },
+    fontFamily: {
+      sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
+      handwriting: ['Caveat', 'cursive'],
+    },
+    fontSize: {
+      ...defaultTheme.fontSize,
+      '10xl': '10rem',
+      '11xl': '11rem',
+      '12xl': '12rem',
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
