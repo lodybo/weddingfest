65ad7de509791deaf1aff1c617ea3b8ed868c74b
Lody Borgers <l.borgers@taf.nl>
2023-06-07 22:32:42 +0200

    Enabled (stable) future flags

--- remix.config.js
+++ remix.config.js
@@ -6,4 +6,11 @@ module.exports = {
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
   devServerPort: 4200,
   tailwind: true,
+  future: {
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  }
 };
